a98dd712cf0d055e36756d082ec700eda7904a10
xiaoyint <xjt@web.de>
2021-10-04 16:31:55 +0000

    adapt Self-GenomeNet option call

--- R/Self-GenomeNet_pretraining/train_Self_GenomeNet.R
+++ R/Self-GenomeNet_pretraining/train_Self_GenomeNet.R
@@ -8,47 +8,50 @@ library(purrr)
 
 train_Self_GenomeNet <-
   function(path,
-           path.val,
-           encoder,
-           context,
-           loss_function,
-           batch.size,
-           epochs,
-           steps.per.epoch,
-           learningrate,
-           run.name,
-           tensorboard.log,
-           maxlen,
-           stepsmin = 1,
-           stepsmax = 10,
-           file_step = NULL,
-           file_max_samples = 64,
-           Self_GenomeNet = TRUE,
-           trained_model = NULL,
-           savemodels = FALSE,
-           proportion_per_file = 0.9,
-           save_every_xth_epoch = 12) {
+    path.val,
+    encoder,
+    context,
+    loss_function,
+    batch.size,
+    epochs,
+    steps.per.epoch,
+    learningrate,
+    run.name,
+    tensorboard.log,
+    maxlen,
+    stepsmin = 1,
+    stepsmax = 10,
+    file_step = NULL,
+    file_max_samples = 64,
+    trained_model = NULL,
+    savemodels = FALSE,
+    proportion_per_file = 0.9,
+    save_every_xth_epoch = 12) {
     # Prepare data
     cat("Preparing the data\n")
-    if(is.null(file_step)){
+    if (is.null(file_step)) {
       file_step <- maxlen
     }
     fastrain <-
-      fastaFileGenerator(path,
-                         batch.size = batch.size,
-                         maxlen = maxlen,
-                         step = fasta_file_step,
-                         max_samples = file_max_samples,
-                         randomFiles = TRUE,
-                         proportion_per_file = proportion_per_file)
+      fastaFileGenerator(
+        path,
+        batch.size = batch.size,
+        maxlen = maxlen,
+        step = fasta_file_step,
+        max_samples = file_max_samples,
+        randomFiles = TRUE,
+        proportion_per_file = proportion_per_file
+      )
     fasval <-
-      fastaFileGenerator(path.val,
-                         batch.size = batch.size,
-                         maxlen = maxlen,
-                         step = fasta_file_step,
-                         max_samples = file_max_samples,
-                         randomFiles = TRUE,
-                         proportion_per_file = proportion_per_file)
+      fastaFileGenerator(
+        path.val,
+        batch.size = batch.size,
+        maxlen = maxlen,
+        step = fasta_file_step,
+        max_samples = file_max_samples,
+        randomFiles = TRUE,
+        proportion_per_file = proportion_per_file
+      )
     
     
     # metrics
@@ -69,18 +72,17 @@ train_Self_GenomeNet <-
             context,
             batch.size = batch.size,
             steps_to_ignore = stepsmin,
-            steps_to_predict = stepsmax,
-            Self_GenomeNet = Self_GenomeNet
-            )
-         )        
+            steps_to_predict = stepsmax
+          )
+        )
     } else {
-        cat(
+      cat(
         format(Sys.time(), "%F %R"),
         ": Loading the trained model; will be compiled manually.\n"
       )
       model <- load_model_hdf5(trained_model)
-    } 
-
+    }
+    
     
     # connect tensorboard
     logdir <- tensorboard.log
@@ -88,15 +90,14 @@ train_Self_GenomeNet <-
     writerval = tf$summary$create_file_writer(file.path(logdir, run.name, "/validation"))
     
     # batch loop
-    training_loop <- function(batches = steps.per.epoch, epoch, Self_GenomeNet = Self_GenomeNet) {
+    training_loop <- function(batches = steps.per.epoch, epoch) {
       #saveloss <- list()
       for (b in seq(batches)) {
         with(tf$GradientTape() %as% tape, {
           a <- fastrain()$X %>% tf$convert_to_tensor()
-          if (isTRUE(Self_GenomeNet)) {
-            a_complement <- tf$convert_to_tensor(array(as.array(a)[, (dim(a)[2]):1, 4:1], dim = c(dim(a)[1], dim(a)[2], dim(a)[3])))
-            a <- tf$concat(list(a, a_complement), axis = 0L)
-          }
+          a_complement <-
+            tf$convert_to_tensor(array(as.array(a)[, (dim(a)[2]):1, 4:1], dim = c(dim(a)[1], dim(a)[2], dim(a)[3])))
+          a <- tf$concat(list(a, a_complement), axis = 0L)
           out <- model(a)
           l <- out[1]
           acc <- out[2]
@@ -114,51 +115,50 @@ train_Self_GenomeNet <-
       
       with(writertrain$as_default(), {
         tf$summary$scalar('epoch_loss',
-                          train_loss$result(),
-                          step = tf$cast(epoch, "int64"))
+          train_loss$result(),
+          step = tf$cast(epoch, "int64"))
         tf$summary$scalar('epoch_accuracy',
-                          train_acc$result(),
-                          step = tf$cast(epoch, "int64"))
+          train_acc$result(),
+          step = tf$cast(epoch, "int64"))
       })
       
       tf$print("Train Loss",
-               train_loss$result(),
-               ", Train Acc",
-               train_acc$result())
+        train_loss$result(),
+        ", Train Acc",
+        train_acc$result())
       
       train_loss$reset_states()
       train_acc$reset_states()
     }
     
-    val_loop <- function(batches = steps.per.epoch, epoch, Self_GenomeNet) {
-      for (b in seq(ceiling(batches*0.1))) {  
+    val_loop <- function(batches = steps.per.epoch, epoch) {
+      for (b in seq(ceiling(batches * 0.1))) {
         a <- fasval()$X %>% tf$convert_to_tensor()
-        if (isTRUE(Self_GenomeNet)) {
-              a_complement <- #a[, tf$convert_to_tensor(seq((dim(a)[2]),1)), tf$convert_to_tensor(seq(4,1))]
-                tf$convert_to_tensor(array(as.array(a)[, (dim(a)[2]):1, 4:1], dim = c(dim(a)[1], dim(a)[2], dim(a)[3])))
-              a <- tf$concat(list(a, a_complement), axis = 0L)
-            }
+        a_complement <-
+          #a[, tf$convert_to_tensor(seq((dim(a)[2]),1)), tf$convert_to_tensor(seq(4,1))]
+          tf$convert_to_tensor(array(as.array(a)[, (dim(a)[2]):1, 4:1], dim = c(dim(a)[1], dim(a)[2], dim(a)[3])))
+        a <- tf$concat(list(a, a_complement), axis = 0L)
         #a_complement <- tf$convert_to_tensor(array(as.array(a)[ , (dim(a)[2]):1, 4:1], dim = c(dim(a)[1],dim(a)[2],dim(a)[3])))
         out <- model(a)
         l <- out[1]
         acc <- out[2]
-       
+        
         val_loss(l)
         val_acc(acc)
       }
       
       with(writerval$as_default(), {
         tf$summary$scalar('epoch_loss',
-                          val_loss$result(),
-                          step = tf$cast(epoch, "int64"))
+          val_loss$result(),
+          step = tf$cast(epoch, "int64"))
         tf$summary$scalar('epoch_accuracy',
-                          val_acc$result(),
-                          step = tf$cast(epoch, "int64"))
+          val_acc$result(),
+          step = tf$cast(epoch, "int64"))
       })
       tf$print("Validation Loss",
-               val_loss$result(),
-               ", Validation Acc",
-               val_acc$result())
+        val_loss$result(),
+        ", Validation Acc",
+        val_acc$result())
       
       val_loss$reset_states()
       val_acc$reset_states()
@@ -169,11 +169,20 @@ train_Self_GenomeNet <-
     cat("Starting Training\n")
     for (i in 1:epochs) {
       cat("Epoch: ", i, " -----------\n")
-      training_loop(epoch = i, Self_GenomeNet = Self_GenomeNet)
-      val_loop(epoch = i, Self_GenomeNet = Self_GenomeNet)
+      training_loop(epoch = i)
+      val_loop(epoch = i)
       if (savemodels) {
         if (i %% save_every_xth_epoch == 0) {
-          model %>% save_model_hdf5(paste("/home/gunduza/projects/cpc_models/", run.name, "_Epoch_",as.array(i), "_temp.h5", sep = ""))
+          model %>% save_model_hdf5(
+            paste(
+              "/home/gunduza/projects/cpc_models/",
+              run.name,
+              "_Epoch_",
+              as.array(i),
+              "_temp.h5",
+              sep = ""
+            )
+          )
           #file.rename(
           #  paste("/home/gunduza/projects/cpc_models/", run.name, "mid_temp.h5", sep = ""),
           #  paste("/home/gunduza/projects/cpc_models/", run.name, "mid.h5", sep = "")
