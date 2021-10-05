8db8596b9d62cad1207956012d32fbd4e00a222d
xiaoyint <xjt@web.de>
2021-10-05 19:11:34 +0000

    adapt supervised code to run properly and rename

---
+++ R/supervised/deepsea/supervised.R
@@ -0,0 +1,51 @@
+library(keras)
+library(deepG)
+library(tensorflow)
+
+model <-
+  load_model_hdf5("pretrained_models/deepsea-1p.h5", compile = FALSE)
+
+model <-
+  remove_add_layers(
+    model,
+    layer_name = model$layers[[5]]$name,
+    freeze_base_model = TRUE,
+    dense_layers = NULL,
+    compile = FALSE
+  )
+
+output_tensor <-
+  model$output %>% layer_flatten() %>% layer_dropout(0.5) %>% #layer_dense(units = 925, activation = "relu") %>% 
+  layer_dense(units = 919, activation = "sigmoid")
+
+model <- tf$keras$Model(inputs = model$input, outputs = output_tensor)
+optimizer <-  optimizer_rmsprop(learning_rate = 0.0001)
+model %>% compile(loss = "binary_crossentropy",
+  optimizer = optimizer,
+  metrics = c("acc"))
+
+trainNetwork(
+  train_type = "label_rds",
+  model = model,
+  path = "data/deepSea/train",
+  path.val = "data/deepSea/validation",
+  checkpoint_path = "checkpoints",
+  validation.split = 0.01,
+  run.name = "semisupervisedpaper_deepsea_acgt_z6_10percent_spe1000_freezed",
+  batch.size = 512,
+  epochs = 100,
+  patience = 50,
+  lr.plateau.factor = 0.1,
+  proportion_per_file = NULL,
+  max_samples = NULL,
+  steps.per.epoch = 1000,
+  tensorboard.log = "tensorboard",
+  output = list(
+    none = FALSE,
+    checkpoints = TRUE,
+    tensorboard = TRUE,
+    log = FALSE,
+    serialize_model = FALSE,
+    full_model = FALSE
+  )
+)
--- R/supervised/virus/supervised.R
+++ R/supervised/virus/supervised.R
@@ -3,7 +3,7 @@ library(deepG)
 library(tensorflow)
 
 model <-
-  load_model_hdf5("pretrained_models/bacteriapretraining.h5", compile = FALSE)
+  load_model_hdf5("pretrained_models/viruspretraining.h5", compile = FALSE)
 
 model <-
   remove_add_layers(
@@ -33,13 +33,13 @@ trainNetwork(
     "data/virus-no-phage-data/validation"
   ),
   checkpoint_path = "checkpoints",
-  run.name = "semisupervised_paper_acgt_z5_bacteriapretrained_train100percent_supervised_spe_bs_vs_linearev",
+  run.name = "semisupervised_paper_acgt_train_10percent_directfinetuning",
   batch.size = 2048,
   epochs = 1000,
   patience = 3,
   cooldown = 1,
-  steps.per.epoch = 400,
-  step = 1000,
+  steps.per.epoch = 1000,
+  step = 150,
   randomFiles = TRUE,
   vocabulary = c("a", "c", "g", "t"),
   tensorboard.log = "tensorboard",
