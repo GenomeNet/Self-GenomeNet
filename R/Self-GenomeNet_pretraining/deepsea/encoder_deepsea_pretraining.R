12ca7a7e180d566ca788162ae74dc0d57779b5d0
Anil Gündüz <67071978+anilgunduz@users.noreply.github.com>
2021-10-05 20:15:49 +0200

    Create encoder_deepsea_pretraining.R

---
+++ R/Self-GenomeNet_pretraining/deepsea/encoder_deepsea_pretraining.R
@@ -0,0 +1,10 @@
+encoder_deepsea_pretraining <- function(maxlen) {
+  input_tensor <- keras::layer_input(shape = c(maxlen, 4))
+  output_tensor <- input_tensor %>%
+    keras::layer_conv_1d(kernel_size = 26,
+      filters = 320,
+      strides = 1)
+  output_tensor <- output_tensor %>% keras::layer_max_pooling_1d(pool_size = 13, strides = 13)
+
+  keras_model(inputs = input_tensor, outputs = output_tensor)
+}
