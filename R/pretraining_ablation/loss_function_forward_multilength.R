9592c22a9e3efc70ea3ad766fe986dafe3f2b112
Anil Gündüz <67071978+anilgunduz@users.noreply.github.com>
2021-11-16 09:32:26 +0100

    Update loss_function_forward_multilength.R

--- R/pretraining_ablation/loss_function_forward_multilength.R
+++ R/pretraining_ablation/loss_function_forward_multilength.R
@@ -13,16 +13,18 @@ loss_function_forward_multilength <- function(latents,
   
   # create context tensor
   ctx <- context(latents)
+  context_length <- ctx$shape[[2]]
   target_dim <- ctx$shape[[3]]
+  labels <- floor(seq(0, (batch.size - 1))) %>% as.integer()  
+  #preds_i <- ctx %>% layer_conv_1d(kernel_size = 1, filters = target_dim)
+  layer <- layer_conv_1d(kernel_size = 1, filters = target_dim)
   # loop for different distances of predicted patches
   for (i in seq(steps_to_ignore, (steps_to_predict - 1), steps_skip)) {
-    target_dim <- ctx$shape[[3]]
-    context_length <- ctx$shape[[2]]
     ctx1 <- ctx[(1:batch.size), ,]
+    ctx1 <- ctx1 %>% layer
     ctx2 <- ctx[(1 + batch.size):(batch.size * 2), ,]
     logits <-
       tf$matmul(ctx1[, context_length - value - steps_to_ignore,], tf$transpose(ctx2[, value,]))
-    labels <- floor(seq(0, (batch.size - 1))) %>% as.integer()
     # calculate loss and accuracy for each step
     loss[[length(loss) + 1]] <-
       tf$nn$sparse_softmax_cross_entropy_with_logits(labels, logits) %>%
