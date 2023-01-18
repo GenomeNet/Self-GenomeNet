bdd65387391f4f204c8426c882446b1c60fcb8a3
Anil Gündüz <67071978+anilgunduz@users.noreply.github.com>
2023-01-18 14:22:45 +0100

    Update context.R

--- R/pretraining/bacteria/context.R
+++ R/pretraining/bacteria/context.R
@@ -1,5 +1,5 @@
 context_bacteria_pretraining <- function(latents) {
   output_tensor <-
-    latents %>% keras::layer_lstm(units = 256, return_sequences = TRUE)
+    latents %>% keras::layer_lstm(units = 512, return_sequences = TRUE)
   return(output_tensor)
 }
