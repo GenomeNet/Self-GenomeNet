7c975a633027ac30415c4a854756c2e68d26f701
Anil Gündüz <67071978+anilgunduz@users.noreply.github.com>
2023-01-27 17:12:27 +0100

    Update context.R

--- R/pretraining/deepsea/context.R
+++ R/pretraining/deepsea/context.R
@@ -1,5 +1,5 @@
-context_deepsea_pretraining <- function(output_tensor) {
-  output_tensor <- output_tensor %>% keras::layer_dropout_lstm(0.2)
-  output_tensor <- output_tensor %>% keras::layer_lstm(units = 320,return_sequences = TRUE)
+context_deepsea_pretraining <- function(latents) {
+  output_tensor <-
+    latents %>% keras::layer_lstm(units = 512, return_sequences = TRUE)
   return(output_tensor)
 }
