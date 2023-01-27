d9aba9fb84b98ed59a3781096f2890adfaa237cf
Anil Gündüz <67071978+anilgunduz@users.noreply.github.com>
2023-01-27 16:53:59 +0100

    Create encoder.R

---
+++ R/pretraining/virus_seqlen1000/encoder.R
@@ -0,0 +1,8 @@
+encoder_pretraining <- function(maxlen) {
+  input_tensor <- keras::layer_input(shape = c(maxlen, 4))
+  output_tensor <- input_tensor %>%
+    keras::layer_conv_1d(kernel_size = 40,
+                         filters = 1024,
+                         strides = 20)
+  keras_model(inputs = input_tensor, outputs = output_tensor)
+}
