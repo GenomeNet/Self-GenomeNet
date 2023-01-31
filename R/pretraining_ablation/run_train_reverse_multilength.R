d2fa6ef7ffbe630edd331b673c930eb80e2ea0d3
Anil Gündüz <67071978+anilgunduz@users.noreply.github.com>
2023-01-31 12:34:14 +0100

    Create run_train_reverse_multilength.R

---
+++ R/pretraining_ablation/run_train_reverse_multilength.R
@@ -0,0 +1,26 @@
+source("R/pretraining_ablation/train_Self_GenomeNet_reverse.R")
+source("R/pretraining/virus_seqlen1000/context.R")
+source("R/pretraining/virus_seqlen1000/encoder.R")
+source("R/pretraining/loss_function_SG.R")
+train_data_folder <- list("data/virus-no-phage-data/train","data/virus-phage-data/train")
+validation_data_folder <- list("data/virus-no-phage-data/validation","data/virus-phage-data/validation")
+tensorboard_folder <- "tensorboard"
+
+train_Self_GenomeNet_reverse_multilength(
+  path            = train_data_folder,
+  path_val        = validation_data_folder,
+  maxlen          = 1000,
+  encoder         = encoder_pretraining(1000),
+  context         = context_pretraining,
+  loss_function   = loss_functionSG,
+  batch_size      = 128,
+  epochs          = 1000,
+  steps_per_epoch = 400,
+  learningrate    = 0.0001,
+  run_name        = paste("Self-GenomeNet_reverse_multi", format(Sys.time(), "_%y%m%d_%H%M"), sep = ""),
+  path_tensorboard = tensorboard_folder,
+  trained_model   = NULL,
+  savemodels      = TRUE,
+  save_every_xth_epoch = 100,
+  proportion_per_seq = 0.9
+)
